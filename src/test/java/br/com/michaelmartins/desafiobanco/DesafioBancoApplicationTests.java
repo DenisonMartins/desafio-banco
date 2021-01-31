f732e73c511e0d938fe61b3f2a4a9a3158d4bc6f
Michael <michael.dl.mrtns@gmail.com>
2021-01-31 08:25:29 -0300

    Commit initial project

---
+++ src/test/java/br/com/michaelmartins/desafiobanco/DesafioBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.michaelmartins.desafiobanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBancoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
